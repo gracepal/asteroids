1597eb0788ff81655c5fa986ba3f673a5c218261
gracepal <gracepal810@gmail.com>
2024-12-19 00:24:52 -0800

    player - groups

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -18,10 +23,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        # player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
 
         screen.fill(color="black")
-        player.draw(screen)
+
+        # player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
